c3accc11dd1e7378738c22d6ceb1861923218eda
FelipeAlcazar <felipe.alcazar@alu.uclm.es>
2024-10-26 19:18:35 +0200

    timeline added

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App h-full">
       <ShineBorder className="relative z-10 p-4 w-full h-screen flex justify-center items-center" borderWidth={3} color="#ff0000"> {/* Add ShineBorder component */}
         <div className="relative z-10 flex flex-col justify-center items-center min-h-full">
           <div className="flex-grow flex flex-col justify-center items-center lg:flex-row">
@@ -77,6 +77,61 @@ function App() {
           </Dock>
         </div>
       </ShineBorder>
+      <section className="relative min-h-screen flex flex-col justify-center bg-gray-100 overflow-hidden"> {/* Updated background class */}
+        <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
+          <div className="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-16">
+            <div className="w-full max-w-3xl mx-auto">
+              <div className="-my-6">
+                <div className="relative pl-8 sm:pl-32 py-6 group">
+                  <div className="font-medium text-red-500 mb-1 sm:mb-0">🎓 Bachillerato</div>
+                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-red-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Jul 2019</time>
+                    <div className="text-xl font-bold text-slate-900">Bachillerato en Ciencias Sociales y Humanidades</div>
+                  </div>
+                  <div className="text-slate-500 italic">1 de septiembre de 2017 - 1 de julio de 2019</div>
+                </div>
+
+                <div className="relative pl-8 sm:pl-32 py-6 group">
+                  <div className="font-medium text-red-500 mb-1 sm:mb-0">&#x1F1EC;&#x1F1E7; Certificación oficial en Inglés</div>
+                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-red-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Nov 2020</time>
+                    <div className="text-xl font-bold text-slate-900">Certificación en lengua inglesa ISE III - CEFR Nivel C1</div>
+                  </div>
+                  <div className="text-slate-500 italic">2 de noviembre de 2020</div>
+                </div>
+                
+                <div className="relative pl-8 sm:pl-32 py-6 group">
+                  <div className="font-medium text-red-500 mb-1 sm:mb-0">🖥️ Ciclo Formativo</div>
+                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-red-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Jun 2021</time>
+                    <div className="text-xl font-bold text-slate-900">Ciclo Formativo de Grado Superior en Desarrollo de Aplicaciones Multiplataforma</div>
+                  </div>
+                  <div className="text-slate-500 italic">1 de septiembre de 2019 - 1 de junio de 2021</div>
+                </div>
+                
+                <div className="relative pl-8 sm:pl-32 py-6 group">
+                  <div className="font-medium text-red-500 mb-1 sm:mb-0">💼 Prácticas Profesionales</div>
+                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-red-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">Jun 2021</time>
+                    <div className="text-xl font-bold text-slate-900">Prácticas profesionales en DIMAX Soluciones Integrales S.L.</div>
+                  </div>
+                  <div className="text-slate-500 italic">Prácticas profesionales en DIMAX Soluciones Integrales S.L., Ciudad Real, España. Participación en el desarrollo de páginas web con WordPress, programación IoT en C++ con placas Arduino, y estudio de investigación de software ERP.</div>
+                  <div className="text-slate-500 italic">7 de abril de 2021 - 18 de junio de 2021</div>
+                </div>
+                
+                <div className="relative pl-8 sm:pl-32 py-6 group">
+                  <div className="font-medium text-red-500 mb-1 sm:mb-0">🧑‍💻 Grado en Ingeniería Informática</div>
+                  <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-red-600 after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
+                    <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-emerald-100 rounded-full">HOY</time>
+                    <div className="text-xl font-bold text-slate-900">Grado en Ingeniería Informática por la Escuela Superior de Informática de UCLM - Cursando 4º curso </div>
+                  </div>
+                  <div className="text-slate-500 italic">1 de septiembre de 2021 - Actualidad</div>
+                </div>
+              </div>                    
+            </div>
+          </div>
+        </div>
+      </section>
       <section id="next-section" className="min-h-screen flex justify-center items-center bg-gray-100">
         <h2 className="text-4xl text-black">More Content Here</h2>
       </section>
